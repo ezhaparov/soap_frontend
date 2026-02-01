794ff5e93fb947635a00dadd1cf80df9a3026b18
Erbol Zhaparov <ae.270718@gmail.com>
2026-02-01 13:32:48 +0600

    change getAll() method to getAllWithFormat() for changing application type

--- src/services/customer.service.js
+++ src/services/customer.service.js
@@ -5,8 +5,19 @@ const API_URL = "/api/customers";
 
 
 class CustomerService {
-  getAll() {
-    return axios.get(API_URL, { headers: authHeader() });
+  getAllWithFormat(format) {
+    const headers = {
+      ...authHeader(),
+      Accept: format === "xml" ? "application/xml" : "application/json",
+    };
+
+    const config = { headers };
+
+    if (format === "xml") {
+      config.responseType = "text";
+    }
+
+    return axios(API_URL, config);
   }
 
   create(data) {
